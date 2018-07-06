8f4028cf045734c868df18714e5e778a4dd75c87
djgovani <deepgovani97@gmail.com>
2018-07-06 11:41:05 +0530

    Added bootstrapVue

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueSweetalert2 from 'vue-sweetalert2';
-
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
+Vue.use(BootstrapVue);
 Vue.use(VueSweetalert2);
 
 /* eslint-disable no-new */
